e2f29f03127835d783a82b2906c7b9c0a73cad15
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-12-22 10:44:31 +0100

    git push

--- js/templates/post.mjs
+++ js/templates/post.mjs
@@ -1,19 +1,45 @@
-export function postTemplateA(postData) {
-  return `<div class="post" id=${postData.id}>${postData.title}</div>`;
-}
-
 export function postTemplateB(postData) {
   const post = document.createElement("div");
   post.classList.add("post");
-  post.innerText = postData.title;
+  post.id = postData.id;
+
+  const title = document.createElement("h2");
+  title.textContent = postData.title;
+  post.appendChild(title);
+
+  const body = document.createElement("p");
+  body.textContent = postData.body;
+  post.appendChild(body);
+
+  const tags = document.createElement("p");
+  tags.textContent = `Tags: ${postData.tags.join(", ")}`;
+  post.appendChild(tags);
+
+  const media = document.createElement("img");
+  media.src = postData.media;
+  media.alt = "Post Media";
+  post.appendChild(media);
+
+  const created = document.createElement("p");
+  created.textContent = `Created: ${postData.created}`;
+  post.appendChild(created);
+
+  const updated = document.createElement("p");
+  updated.textContent = `Last Updated: ${postData.updated}`;
+  post.appendChild(updated);
+
+  const count = document.createElement("p");
+  count.textContent = `Comments: ${postData._count.comments}, Reactions: ${postData._count.reactions}`;
+  post.appendChild(count);
+
   return post;
 }
 
 export function renderPostTemplate(postData, parent) {
-  //parent.innerHTML = postTemplate(postData);
   parent.append(postTemplateB(postData));
 }
 
 export function renderPostTemplates(postDataList, parent) {
-  parent.append(...postDataList.map(postTemplateB));
+  const posts = postDataList.map((postData) => postTemplateB(postData));
+  parent.append(...posts);
 }
