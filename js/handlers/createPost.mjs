77a9d65d3ab18274024b6da701dcc7d27a463435
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-12-22 23:05:19 +0100

    git push

--- js/handlers/createPost.mjs
+++ js/handlers/createPost.mjs
@@ -7,14 +7,22 @@ export function setCreatePostListener() {
   const id = url.searchParams.get("id");
 
   if (form) {
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
       const post = Object.fromEntries(formData.entries());
       post.id = id;
 
-      createPost(post);
+      try {
+        // Attempt to create the post
+        await createPost(post);
+        // Handle the post creation success here (e.g., redirect, show success message)
+      } catch (error) {
+        // Handle the error here (e.g., display error message to the user)
+        console.error("Error creating post:", error);
+        // Optionally, inform the user of the error
+      }
     });
   }
 }
